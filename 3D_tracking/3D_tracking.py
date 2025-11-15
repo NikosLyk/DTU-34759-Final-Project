4816aacd55534ce1bdf3a15949e19f73d0619059
sijojen <sijojensen@gmail.com>
2025-11-15 13:03:58 +0100

    Added a kalman class, containing kalman functions, as well as kalman parameters, for kalman filtering

---
+++ 3D_tracking/3D_tracking.py
@@ -0,0 +1,3 @@
+import cv2
+import numpy as np
+import kalman_filter
---
+++ 3D_tracking/kalman_filter.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+
+class kalman:
+
+    def __init__(self, initial_position, dt):
+        self.position = initial_position
+        self.delta_time = dt
+        self.age = 0   # Frames this instance has been alive
+        self.missed_frames = 0
+
+        # The initial state vector (x, y, z, vx, vy, vz) (6x1).
+        self.x = np.array([[initial_position[0]],
+                      [initial_position[1]],
+                      [initial_position[2]],
+                      [0],
+                      [0],
+                      [0]])
+
+        # The initial uncertainty (6x6).
+        self.P = np.array([[1000000000000, 0, 0, 0, 0, 0],
+                      [0, 1000000000000, 0, 0, 0, 0],
+                      [0, 0, 1000000, 0, 0, 0],
+                      [0, 0, 0, 1000000, 0, 0],
+                      [0, 0, 0, 0, 1000000, 0],
+                      [0, 0, 0, 0, 0, 1000000]])
+
+        # The external motion (6x1).
+        self.u = np.array([[0],
+                      [0],
+                      [0],
+                      [0],
+                      [0],
+                      [0]])
+
+        # The transition matrix (6x6). NB: NOT LIKE THIS
+        self.F = np.array([[1, 0, dt, 0, 0.5 * dt ** 2, 0],
+                      [0, 1, 0, dt, 0, 0.5 * dt ** 2],
+                      [0, 0, 1, 0, dt, 0],
+                      [0, 0, 0, 1, 0, dt],
+                      [0, 0, 0, 0, 1, 0],
+                      [0, 0, 0, 0, 0, 1]])
+
+        # The observation matrix (2x6). NB: NOT LIKE THIS
+        self.H = np.array([[1, 0, 0, 0, 0, 0],
+                      [0, 1, 0, 0, 0, 0]])
+
+        # The measurement uncertainty.
+        self.R = np.array([[1000000, 0],
+                      [0, 1000000]])
+
+        self.I = np.array([[1, 0, 0, 0, 0, 0],
+                      [0, 1, 0, 0, 0, 0],
+                      [0, 0, 1, 0, 0, 0],
+                      [0, 0, 0, 1, 0, 0],
+                      [0, 0, 0, 0, 1, 0],
+                      [0, 0, 0, 0, 0, 1]])
+
+    def update(self, Z):
+        y = Z - np.dot(self.H, self.x)
+        S = np.dot(np.dot(self.H, self.P), np.transpose(self.H)) + self.R
+        K = np.dot(np.dot(self.P, np.transpose(self.H)), np.linalg.pinv(S))
+        self.x = self.x + np.dot(K, y)
+        P = np.dot(self.I - np.dot(K, self.H), self.P)
+
+
+    def predict(self):
+        self.x = np.dot(self.F, self.x) + self.u
+        self.P = np.dot(np.dot(self.F, self.P), np.transpose(self.F))
